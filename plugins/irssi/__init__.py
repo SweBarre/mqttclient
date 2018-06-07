4e50b6bb115b8e0b955f165eaeb05a3ea726b431
Jonas Forsberg <jonas.forsberg@suse.com>
2018-06-07 13:44:00 +0200

    fixed spacing

--- plugins/irssi/__init__.py
+++ plugins/irssi/__init__.py
@@ -7,7 +7,7 @@ def init(config=None):
 
 def prep(msg=None):
     return_dict = {}
-    return_dict['runner']='notify'
+    return_dict['runner'] = 'notify'
     if msg is not None:
         return_dict['title'], return_dict['message'] = msg.payload.split(":", 1)
         return_dict['icon'] = ICON
