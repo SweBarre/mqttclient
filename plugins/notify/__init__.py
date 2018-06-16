3572826210b0eed071c249e06cc52b5578f4a88a
Jonas Forsberg <jonas.forsberg@suse.com>
2018-06-16 13:04:04 +0200

    Fixes #9 added urgency and icon config to notify

--- plugins/notify/__init__.py
+++ plugins/notify/__init__.py
@@ -14,12 +14,27 @@ pynotify.init("MQTTClient")
 def prep(msg=None, config=None):
     return_dict = {}
     return_dict['config'] = config
+    icon = ICON
+    if config is not None:
+        if 'icon' in config:
+            icon = config['icon']
     if msg is not None:
-        return_dict = {'title': msg.topic, 'message': msg.payload, 'icon': ICON }
+        return_dict['title'] = msg.topic
+        return_dict['message'] = msg.payload
+        return_dict['icon'] = icon
     return return_dict
     
 
 def run(**kwargs):
+    priorities = {
+            'low': pynotify.URGENCY_LOW,
+            'normal': pynotify.URGENCY_NORMAL,
+            'critical': pynotify.URGENCY_CRITICAL
+            }
+    priority = 'normal'
+    if kwargs['config'] is not None:
+        if 'urgency' in kwargs['config']:
+            priority = kwargs['config']['urgency']
     notification = pynotify.Notification(kwargs['title'], kwargs['message'], kwargs['icon'])
-    notification.set_urgency(pynotify.URGENCY_CRITICAL)
+    notification.set_urgency(priorities.get(priority))
     notification.show()
