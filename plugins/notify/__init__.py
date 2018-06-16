48300865b8ea387688c645070faf7993e08d258b
Jonas Forsberg <jonas.forsberg@suse.com>
2018-06-16 12:38:17 +0200

    added config to plugin prep function

--- plugins/notify/__init__.py
+++ plugins/notify/__init__.py
@@ -1,11 +1,19 @@
+"""
+Plugin that uses libnotify to display mqtt subscriptoins
+
+config:
+    urgency      low, normal or critical
+    icon         absolute path to the icon
+"""
 import os
 import pynotify
 
 ICON = os.path.dirname(os.path.realpath(__file__))+'/icon.png'
 pynotify.init("MQTTClient")
 
-def prep(msg=None):
+def prep(msg=None, config=None):
     return_dict = {}
+    return_dict['config'] = config
     if msg is not None:
         return_dict = {'title': msg.topic, 'message': msg.payload, 'icon': ICON }
     return return_dict
